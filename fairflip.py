352ebfc2027cb17c5665ccd13272abf064846c6b
Vi Grey <vi@vigrey.com>
2017-10-12 21:58:22 -0400

    Add code comments and LICENSE

--- fairflip.py
+++ fairflip.py
@@ -1,14 +1,15 @@
 import random
 import sys
 
-heads_percent = int(sys.argv[1])
+heads_percent = int(sys.argv[1]) # Integer value of argument
 
-while heads_percent > 0 and heads_percent < 100:
-    f1 = heads_percent > random.randint(0, 100)
-    f2 = heads_percent > random.randint(0, 100)
-    if f1 != f2:
-        if f1:
+while heads_percent > 0 and heads_percent < 100: # Loop if valid heads_percent
+    # If random number is under heads_percent, then it is HEADS
+    flip1 = heads_percent > random.randint(0, 100) # True if HEADS result
+    flip2 = heads_percent > random.randint(0, 100) # True if HEADS result
+    if flip1 != flip2: # Continue loop if flip1 and flip2 are the same
+        if flip1: # If first flip is HEADS
             print("HEADS")
-        else:
+        else: # If first flip is TAILS
             print("TAILS")
-        break
+        break # break out of infinite loop
